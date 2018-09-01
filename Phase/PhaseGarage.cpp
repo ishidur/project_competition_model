6d620c068d85feae816d331d7a20d15fddc76f69
Ryota Ishidu <ryota6621@gmail.com>
2018-09-01 19:09:20 +0900

    とりあえず移行

--- Phase/PhaseGarage.cpp
+++ Phase/PhaseGarage.cpp
@@ -1,14 +1,156 @@
 #include "PhaseGarage.h"
-#include <stdio.h>
+
+#include "stdlib.h"
+#include "../BaseHardware/Timer.h"
+#include "../DrivingControl/PoseDrivingControl.h"
+#include "../AppliedHardware/VehicleHardware/Tail.h"
+#include "../AppliedHardware/VehicleHardware/PostureSensor.h"
+#include "../AppliedHardware/VehicleHardware/DriveWheels.h"
+#include "../Positioning/Localization/SelfPos.h"
+#include "../Utilities/Vector2D.h"
 
 using namespace Phase;
+using namespace DrivingControl;
+using namespace AppliedHardware::VehicleHardware;
+using namespace BaseHardware;
+using namespace Positioning::Localization;
+using namespace Utilities;
+
 
 PhaseGarage::PhaseGarage(){
+    pos = SelfPos::GetInstance();
 }
 
 void PhaseGarage::Execute(){
-	printf("PhaseGarage Execute done\n");
+	printf("PhaseGarage Execute\n");
+    PoseDrivingControl poseDrivingControl;
+
+    Tail* tail = Tail::GetInstance();
+    Timer* timer = Timer::GetInstance();
+
+    PostureSensor postureSensor; 
+    DriveWheels* driveWheels = DriveWheels::GetInstance();
+
+    FILE* file;
+    for (int i = 0; ; i++) {
+        char filename[255] = {};
+        sprintf(filename, "/ev3rt/res/log_data_endstop_%03d.csv", i);
+        file = fopen(filename, "r");    // ファイルが無いときNULL
+        if (file == NULL) {
+            fclose(file);
+            // ファイルを作成する
+            file = fopen(filename, "w");
+            break;
+        }
+        fclose(file);
+    }
+    fprintf(file,"t,gyroSensor,angleLeft,angleRight,pwmLeft,pwmRight,posSelf.x,posSelf.y,thetaSelf\n");
+
+    int frameCount = 0;
+    const int log_refleshrate = 15;
+
+    float angleLeft=0.0, angleRight=0.0;
+    signed char pwmLeft=0, pwmRight=0;
+
+    timer->Reset();
+
+    Vector2D startPos = pos->GetSelfPos();
+    Vector2D posSelf(0,0);
+    float thetaSelf=0.0;
     
+    pos->UpdateSelfPos();
+    posSelf = pos->GetSelfPos();
+    thetaSelf = pos->GetTheta();
+
+    // 起き上がり
+    while(true){
+        if (abs(tail->GetAngle() - 75) < 2) {
+            tslp_tsk(400);
+            poseDrivingControl.SetParams(30,0,45,true);
+            poseDrivingControl.Driving();
+            tslp_tsk(400);
+            break;
+        }
+        poseDrivingControl.SetParams(-10,0,75,false);
+        poseDrivingControl.Driving();
+
+        pos->UpdateSelfPos();
+        posSelf = pos->GetSelfPos();
+        thetaSelf = pos->GetTheta();
+
+        if ((frameCount++) % log_refleshrate == 0) {
+            driveWheels->GetAngles(&angleLeft, &angleRight);
+            driveWheels->GetPWMs(&pwmLeft, &pwmRight);
+            fprintf(file,"%f,%f,%lf,%lf,%d,%d,%lf,%lf,%lf\n",
+                timer->Now(),postureSensor.GetAnglerVelocity(), angleLeft, angleRight, pwmLeft, pwmRight, posSelf.x, posSelf.y, thetaSelf);
+        }
+        
+        tslp_tsk(4);
+    }
+    ev3_speaker_play_tone(440, 50);
+    // 前進
+    int tau = 1000 + timer->Now();
+    while (timer->Now() < tau) {
+        poseDrivingControl.SetParams(70,0,45,true);
+        poseDrivingControl.Driving();
+
+        pos->UpdateSelfPos();
+        posSelf = pos->GetSelfPos();
+        thetaSelf = pos->GetTheta();
+
+        if ((frameCount++) % log_refleshrate == 0) {
+            driveWheels->GetAngles(&angleLeft, &angleRight);
+            driveWheels->GetPWMs(&pwmLeft, &pwmRight);
+            fprintf(file,"%f,%f,%lf,%lf,%d,%d,%lf,%lf,%lf\n",
+                timer->Now(),postureSensor.GetAnglerVelocity(), angleLeft, angleRight, pwmLeft, pwmRight, posSelf.x, posSelf.y, thetaSelf);
+        }
+        tslp_tsk(4);
+    }
+
+    tau = 1000 + timer->Now();
+    while (timer->Now() < tau) {
+        poseDrivingControl.SetParams(0,0,45,true);
+        poseDrivingControl.Driving();
+
+        pos->UpdateSelfPos();
+        posSelf = pos->GetSelfPos();
+        thetaSelf = pos->GetTheta();
+
+        if ((frameCount++) % log_refleshrate == 0) {
+            driveWheels->GetAngles(&angleLeft, &angleRight);
+            driveWheels->GetPWMs(&pwmLeft, &pwmRight);
+            fprintf(file,"%f,%f,%lf,%lf,%d,%d,%lf,%lf,%lf\n",
+                timer->Now(),postureSensor.GetAnglerVelocity(), angleLeft, angleRight, pwmLeft, pwmRight, posSelf.x, posSelf.y, thetaSelf);
+        }
+        
+        tslp_tsk(4);
+    }
+
+    while(true){
+        if (abs(tail->GetAngle() - 70) < 2) {
+            poseDrivingControl.SetStop(true,false,false);
+            poseDrivingControl.SetParams(30,0,70,false);
+            poseDrivingControl.Driving();
+            tslp_tsk(300);
+            poseDrivingControl.SetStop(true,true,true);
+            break;
+        }
+        poseDrivingControl.SetParams(0,0,70,true);
+        poseDrivingControl.Driving();
+
+        pos->UpdateSelfPos();
+        posSelf = pos->GetSelfPos();
+        thetaSelf = pos->GetTheta();
+
+        if ((frameCount++) % log_refleshrate == 0) {
+            driveWheels->GetAngles(&angleLeft, &angleRight);
+            driveWheels->GetPWMs(&pwmLeft, &pwmRight);
+            fprintf(file,"%f,%f,%lf,%lf,%d,%d,%lf,%lf,%lf\n",
+                timer->Now(),postureSensor.GetAnglerVelocity(), angleLeft, angleRight, pwmLeft, pwmRight, posSelf.x, posSelf.y, thetaSelf);
+        }
+        
+        tslp_tsk(4);
+    }
 	finFlg = true;
 	printf("PhaseGarage Execute done\n");
 }
--- Phase/PhaseGarage.h
+++ Phase/PhaseGarage.h
@@ -1,10 +1,13 @@
 #ifndef PHASE_PHASE_GARAGE_H
 #define PHASE_PHASE_GARAGE_H
 
+#include "../Positioning/Localization/SelfPos.h"
 #include "PhaseBase.h"
 
 namespace Phase{
 	class PhaseGarage : public PhaseBase{
+        private:
+            Positioning::Localization::SelfPos* pos;
 		public:
 			PhaseGarage();
 			void Execute() override;
