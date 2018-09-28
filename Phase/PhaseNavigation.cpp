67638adce6cdab9248db545013cd2ceeb67075ed
ataronus <im2tb4njto3zdcl@gmail.com>
2018-09-29 05:22:40 +0900

    マージ

--- Phase/PhaseNavigation.cpp
+++ Phase/PhaseNavigation.cpp
@@ -23,6 +23,8 @@ using namespace EnvironmentMeasurement;
 using namespace Positioning::Localization;
 using namespace Utilities;
 
+#define LOG_NAVI
+
 #define TAIL_ANGLE (50)
 
 PhaseNavigation::PhaseNavigation(){
@@ -56,7 +58,7 @@ void PhaseNavigation::Execute(){
     fprintf(file,"clock,caribratedBrightness,gyroSensor,USdist,power,turn,PWMt,motor_ang_t,PWMl,PWMr,motor_ang_l,motor_ang_r,xEst,yEst,thetaSelf\n");
 
     int frameCount = 0;
-    const int log_refleshrate = 15;
+    const int log_refleshrate = 0;
 	int com_count = 0;
 
 
@@ -95,13 +97,13 @@ void PhaseNavigation::Execute(){
 #endif
 		now_gyro = postureSensor.GetAnglerVelocity();
 
-#ifdef LOG
+#ifdef LOG_NAVI
 		if ((frameCount++) > log_refleshrate) {
 			driveWheels->GetAngles(&angleLeft, &angleRight);
 			driveWheels->GetPWMs(&pwmLeft, &pwmRight);
 			fprintf(file,"%f,%f,%f,%f,%f,%f,%d,%f,%d,%d,%f,%f,%f,%f,%f\n",
 				cl->GetValue(), envViewer->GetLuminance(), postureSensor.GetAnglerVelocity(),envViewer->GetUSDistance(),
-				0,0,
+				0.0,0.0,
 				tail->GetPWM(),tail->GetAngle(),
 				pwmLeft, pwmRight, angleLeft, angleRight,
 				posSelf.x, posSelf.y, thetaSelf);
@@ -137,16 +139,16 @@ void PhaseNavigation::Execute(){
 	float pre_foward = 150;
 	poseDrivingControl.SetParams(pre_foward,0,TAIL_ANGLE,true);
 	driveWheels->GetAngles(&angleLeft, &angleRight);
-
     while (true){
 		poseDrivingControl.Driving();
 
 		pos->UpdateSelfPos();
 		posSelf = pos->GetSelfPos();
 		thetaSelf = pos->GetTheta();
-
+		
+		driveWheels->GetAngles(&angleLeft, &angleRight);
+#ifdef LOG_NAVI
 		if ((frameCount++) > log_refleshrate) {
-			driveWheels->GetAngles(&angleLeft, &angleRight);
 			driveWheels->GetPWMs(&pwmLeft, &pwmRight);
 			fprintf(file,"%f,%f,%f,%f,%f,%f,%d,%f,%d,%d,%f,%f,%f,%f,%f\n",
 				cl->GetValue(), envViewer->GetLuminance(), postureSensor.GetAnglerVelocity(),envViewer->GetUSDistance(),
@@ -156,8 +158,9 @@ void PhaseNavigation::Execute(){
 				posSelf.x, posSelf.y, thetaSelf);
 			frameCount = 0;
 		}
+#endif
 
-		if(posSelf.DistanceFrom(startPos)>7){
+		if(posSelf.DistanceFrom(startPos)>7&&angleRight>100){
 			break;
 		}
 
@@ -168,46 +171,71 @@ void PhaseNavigation::Execute(){
 	printf("PhaseNavigation 3.Linetrace\n");
 	LineLuminance line;
 	ExponentialSmoothingFilter expFilter(0.5,150.0);
-<<<<<<< HEAD
-	float tmp_forward = 100;
-	while(1){
-		pos->UpdateSelfPos();
-		posSelf = pos->GetSelfPos();
-		thetaSelf = pos->GetTheta();
+	float tmp_forward = 70;
 
-=======
-    int fileterSize = 100;
+    int filterSize = 20;
     int cnt = 0;
-	float forward;
-    float caribratedBrightnesses[fileterSize]={0.0};
-    float greyBottom = 45.0;
-    float greyTop = 55.0;
+	bool max_flg = false;
+    float caribratedBrightnesses[filterSize]={0.0};
+    Vector2D poses[filterSize];
+    float greyBottom = 42.0;
+    float greyTop = 48.0;
     float varianceCriteria = 5.0;
-    while (true) {        
->>>>>>> 分散値による
+	float variance, sum, squaredSum;
+	float nowluminance, lastluminance = envViewer->GetLuminance(), delta;
+	cl->Reset();
 		line.CalcTurnValue();
 		turn = line.GetTurn();
 		poseDrivingControl.SetParams(tmp_forward,turn,TAIL_ANGLE,true);
 		poseDrivingControl.Driving();
 
+		nowluminance = envViewer->GetLuminance();
+        caribratedBrightnesses[cnt] = nowluminance;
+		poses[cnt] = posSelf;
+		// lastluminance = nowluminance;
+		if(cnt==filterSize-1&&cl->Now()>2000) max_flg = true;
+
+        sum=0.0;
+        squaredSum=0.0;
+        for (int i = cnt; i < filterSize/2;i++) {
+			delta = (caribratedBrightnesses[i]-caribratedBrightnesses[(i-10)%filterSize]);
+			delta = delta*delta;
+			sum += delta;
+			squaredSum += delta*delta;
+        }
+		sum /= filterSize;
+        variance = squaredSum/filterSize - sum*sum;
+		if (max_flg&&variance<varianceCriteria &&
+				greyBottom<nowluminance&&nowluminance<greyTop && 
+				poses[cnt].DistanceFrom(poses[(cnt-10)%filterSize])>3){
+            ev3_speaker_play_tone(NOTE_C4, 100);
+			poseDrivingControl.SetParams(0,0,TAIL_ANGLE,true);
+            while(1){
+				poseDrivingControl.Driving();
+			}
+        }
+		cnt = (cnt+1)%filterSize;
+
+#ifdef LOG_NAVI
 		if ((frameCount++) > log_refleshrate) {
 			driveWheels->GetAngles(&angleLeft, &angleRight);
 			driveWheels->GetPWMs(&pwmLeft, &pwmRight);
-			fprintf(file,"%f,%f,%f,%f,%f,%f,%d,%f,%d,%d,%f,%f,%f,%f,%f\n",
+			fprintf(file,"%f,%f,%f,%f,%f,%f,%d,%f,%d,%d,%f,%f,%f,%f,%f,%f\n",
 				cl->GetValue(), envViewer->GetLuminance(), postureSensor.GetAnglerVelocity(),envViewer->GetUSDistance(),
 				tmp_forward,turn,
 				tail->GetPWM(),tail->GetAngle(),
 				pwmLeft, pwmRight, angleLeft, angleRight,
-				posSelf.x, posSelf.y, thetaSelf);
+				posSelf.x, posSelf.y, thetaSelf, variance);
 			frameCount = 0;
 		}
+#endif
 
-		if((com_count++) > 15){
-			char mes[255];
-			sprintf(mes, "{'x':%f,'y':%f,'theta':%f}\n", posSelf.x, posSelf.y, thetaSelf);
-			com->SendString(mes);
-			com_count = 0;
-		}
+		// if((com_count++) > 15){
+		// 	char mes[255];
+		// 	sprintf(mes, "{'x':%f,'y':%f,'theta':%f}\n", posSelf.x, posSelf.y, thetaSelf);
+		// 	com->SendString(mes);
+		// 	com_count = 0;
+		// }
 
 		tslp_tsk(4);
 	}
